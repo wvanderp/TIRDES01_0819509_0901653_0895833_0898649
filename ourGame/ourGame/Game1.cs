4d5da465449e0cefb6a0fb510fb6bcabeb08bd63
Arco Dielhof <Veltrex@live.nl>
2015-09-09 19:21:08 +0200

    Derp
    Viper MK2 added and some code for it to show?

--- ourGame/ourGame/Game1.cs
+++ ourGame/ourGame/Game1.cs
@@ -2,57 +2,52 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace ourGame
-{
+namespace ourGame {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
-    {
+
+    public class Game1 : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        private Texture2D viper;
 
-        public Game1()
-        {
+        public Game1() {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
-        protected override void Initialize()
-        {
+        protected override void Initialize() {
             // TODO: Add your initialization logic here
 
             base.Initialize();
         }
 
-        protected override void LoadContent()
-        {
+        protected override void LoadContent() {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            viper = Content.Load<Texture2D>("ViperMK2.png");
             // TODO: use this.Content to load your game content here
         }
 
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here DERP!
+        protected override void UnloadContent() {
+            // TODO: Unload any non ContentManager content here
         }
 
-        protected override void Update(GameTime gameTime)
-        {
+        protected override void Update(GameTime gameTime) {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.W)) {
+            //
+            //}
+            
 
             base.Update(gameTime);
         }
 
-        protected override void Draw(GameTime gameTime)
-        {
+        protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
