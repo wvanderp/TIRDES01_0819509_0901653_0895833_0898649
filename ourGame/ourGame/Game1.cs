ef155cc2bba4d28751ca0ba526d3aaec1eab2257
neurocinetics <0901653@student.hr.nl>
2015-09-09 22:06:36 +0200

    movementz, adding content to solution.

--- ourGame/ourGame/Game1.cs
+++ ourGame/ourGame/Game1.cs
@@ -3,14 +3,14 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace ourGame {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
 
     public class Game1 : Game {
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         private Texture2D viper;
+        Vector2 playerPosition;
+        private KeyboardState keyboardState;
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
@@ -18,41 +18,46 @@ namespace ourGame {
         }
 
         protected override void Initialize() {
-            // TODO: Add your initialization logic here
-
+            playerPosition = new Vector2(250, 250);
             base.Initialize();
         }
 
         protected override void LoadContent() {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
             viper = Content.Load<Texture2D>("ViperMK2.png");
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void UnloadContent() {
-            // TODO: Unload any non ContentManager content here
         }
 
         protected override void Update(GameTime gameTime) {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+
+            keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Escape))
                 Exit();
-            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.W)) {
-            //
-            //}
-            
+
+            var playerDelta = Vector2.Zero;
+            if (keyboardState.IsKeyDown(Keys.Left))
+                playerDelta -= new Vector2(1.0f, 0.0f);
+            if (keyboardState.IsKeyDown(Keys.Right))
+                playerDelta -= new Vector2(-1.0f, 0.0f);
+            if (keyboardState.IsKeyDown(Keys.Up))
+                playerDelta -= new Vector2(0.0f, 1.0f);
+            if (keyboardState.IsKeyDown(Keys.Down))
+                playerDelta -= new Vector2(0.0f, -1.0f);
+
+            playerPosition += playerDelta * 4.5f;
 
             base.Update(gameTime);
-        }
+            }
 
         protected override void Draw(GameTime gameTime) {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            GraphicsDevice.Clear(Color.Black);
 
             spriteBatch.Begin();
-
-            spriteBatch.Draw(viper, new Vector2(500, 500), Color.White);
-
+            spriteBatch.Draw(viper, playerPosition, Color.White);
             spriteBatch.End();
 
             base.Draw(gameTime);
