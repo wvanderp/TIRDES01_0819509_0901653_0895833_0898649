df72fce0b35b01180e744c40e1e919b9d707e970
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2015-09-10 14:30:04 +0200

    Added inertia and rotation to ship, added an Ship class and instantiated it in Game.cs.

--- ourGame/ourGame/Game1.cs
+++ ourGame/ourGame/Game1.cs
@@ -9,12 +9,9 @@ namespace ourGame {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        private KeyboardState keyboardState;
-        Vector2 playerPosition;
-        Vector2 laserBeamPosition;
+        Ship ship;
 
-        private Texture2D viper;
-        private Texture2D laserBeam;
+
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
@@ -22,14 +19,12 @@ namespace ourGame {
         }
 
         protected override void Initialize() {
-            playerPosition = new Vector2(250, 250);
             base.Initialize();
         }
 
         protected override void LoadContent() {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            viper = Content.Load<Texture2D>("ViperMK2.1s.png");
-            laserBeam = Content.Load<Texture2D>("laserBeam.png");
+            ship = new Ship(Content.Load<Texture2D>("ViperMK2.1s.png"));
         }
 
         protected override void UnloadContent() {
@@ -37,47 +32,16 @@ namespace ourGame {
 
         protected override void Update(GameTime gameTime) {
 
-            keyboardState = Keyboard.GetState();
+            KeyboardState keyboardState = Keyboard.GetState();
 
             if (keyboardState.IsKeyDown(Keys.Escape))
                 Exit();
 
-            var playerDelta = Vector2.Zero;
-            if (keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A)) {
-                playerDelta -= new Vector2(1.0f, 0.0f);
-            }
-
-            if (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D)) {
-                playerDelta -= new Vector2(-1.0f, 0.0f);
-            }
-
-            if (keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W)) {
-                playerDelta -= new Vector2(0.0f, 1.0f);
-            }
-
-            if (keyboardState.IsKeyDown(Keys.Down) || keyboardState.IsKeyDown(Keys.S)) {
-                playerDelta -= new Vector2(0.0f, -1.0f);
-                
-            }
-
-            playerPosition += playerDelta * 2.5f;
+            ship.Update(keyboardState);
 
             if (keyboardState.IsKeyDown(Keys.Space)) {
-                //for (laserBeamPosition = (playerPosition + new Vector2(12.0f, 30.0f)); laserBeamPosition.Y < 900.0f; laserBeamPosition =  (laserBeamPosition + new Vector2(0.0f, 1.0f))) {
-                //    System.Diagnostics.Debug.Write("HerpDerp prior to printstuff\n");
-                //    System.Diagnostics.Debug.Write(laserBeamPosition);
-                //    System.Diagnostics.Debug.Write("HerpDerp prior to spritBatch.Begin()\n");
-                //    spriteBatch.Begin();
-                //    System.Diagnostics.Debug.Write("HerpDerp post to spritBatch.Begin()\n");
-                //    spriteBatch.Draw(laserBeam, laserBeamPosition, Color.White);
-                //              offset of current player position: Vector2(12.0f, 30.0f)
-                //      remove something if the beam gets too long (like 6 in a row is fine but when above line creates a 7th, the 1st is removed again)
-                //    System.Diagnostics.Debug.Write("HerpDerp prior to spritBatch.End()\n");
-                //    spriteBatch.End();
-                //    System.Diagnostics.Debug.Write("HerpDerp post to spritBatch.End()\n");
-                //}
+           
             }
-            //System.Diagnostics.Debug.Print("", playerPosition.X);
 
             base.Update(gameTime);
         }
@@ -87,8 +51,7 @@ namespace ourGame {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-            spriteBatch.Draw(viper, playerPosition, Color.White);
-            spriteBatch.Draw(laserBeam, (playerPosition + new Vector2(12.0f, 30.0f)), Color.White);
+            ship.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ ourGame/ourGame/Ship.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ourGame
+{
+    class Ship
+    {
+        Texture2D texture;
+        bool inertiaDampenersEnabled = false;
+        float heading = 0.0f;
+        float speed = 0.0f;
+        Vector2 velocity = new Vector2(0, 0);
+        Rectangle position = new Rectangle(300, 200, 50, 50);
+        bool spacebarPressed = false;
+        float x= 400.0f, y = 350.0f;
+        public Ship(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+        public void Update(KeyboardState keyboardState)
+        {
+            if(keyboardState.IsKeyDown(Keys.Left)) {
+                heading -= 3.0f / 60;
+                System.Console.WriteLine("Rotation: {0}", heading - MathHelper.Pi);
+            }
+            if(keyboardState.IsKeyDown(Keys.Right)) {
+                heading += 3.0f / 60;
+                System.Console.WriteLine("Rotation: {0}", heading - MathHelper.Pi);
+            }
+            if (keyboardState.IsKeyDown(Keys.Up)) {
+                
+                if (!inertiaDampenersEnabled) {
+                    velocity.X += (float)System.Math.Sin((double)heading) * .3f;
+                    velocity.Y += (float)System.Math.Cos((double)heading) * .3f * -1;
+                } else {
+                    speed += .3f;
+                }
+            }
+            if (keyboardState.IsKeyDown(Keys.Down)) {
+                if (!inertiaDampenersEnabled) {
+                    velocity.X -= (float)System.Math.Sin((double)heading) * .3f;
+                    velocity.Y -= (float)System.Math.Cos((double)heading) * .3f * -1;
+                }
+                else {
+                    speed -= .1f;
+                }
+            }
+            if (keyboardState.IsKeyDown(Keys.Space)) {
+                if (!spacebarPressed) {
+                    spacebarPressed = true;
+                    if (inertiaDampenersEnabled) {
+                        inertiaDampenersEnabled = false;
+                        velocity.X = (float)System.Math.Sin((double)heading) * speed;
+                        velocity.Y = (float)System.Math.Cos((double)heading) * speed * -1;
+                    }
+                    else {
+                        inertiaDampenersEnabled = true;
+                    }
+                }
+                
+            }
+            if (keyboardState.IsKeyUp(Keys.Space)) {
+                spacebarPressed = false;
+            }
+            if (keyboardState.IsKeyDown(Keys.Tab)) {
+                speed = 0.0f;
+                x = 0.0f;
+                y = 0.0f;
+            }
+            if (heading > MathHelper.Pi * 2) {
+                heading -= MathHelper.Pi * 2;
+            } else if (heading < 0){
+                heading += MathHelper.Pi * 2;
+            }
+            if (inertiaDampenersEnabled) {
+                x += (float)System.Math.Sin((double)heading) * speed;
+                y += (float)System.Math.Cos((double)heading) * speed * -1;
+            }
+            else {
+                x += velocity.X;
+                y += velocity.Y;
+            }
+            position.Y = (int)y;
+            position.X = (int)x;            
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, sourceRectangle: null, color: Color.White, rotation: heading, origin: new Vector2(texture.Width / 2, texture.Height / 2),effects: SpriteEffects.None, layerDepth: 1.0f);
+            // TODO: Add your drawing code here
+        }
+    }
+}
