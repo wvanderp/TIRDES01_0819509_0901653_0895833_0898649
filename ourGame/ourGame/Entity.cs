0e5777740a95375d47fa4c1a578dff1694f80454
neurocinetics <0901653@student.hr.nl>
2015-10-23 14:49:15 +0200

    Implemented Entity Class, next up... moving code from the classes: Ship, Projectile and CylonRaiders into the update method within the Game class, with the help of an input controller. After these steps the Ship, CylonRaiders and Projectile classes can be erased.

---
+++ ourGame/ourGame/Entity.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
+using Microsoft.Xna.Framework.Content;
+
+struct Entity
+{
+    public Entity(Vector2 position, Texture2D appearance, float heading, float speed)
+    {
+        Position = position;
+        Appearance = appearance;
+        Heading = heading;
+        Speed = speed;
+    }
+
+    public Vector2 Position
+    {
+        get; private set;
+    }
+    public Texture2D Appearance
+    {
+        get; private set;
+    }
+    public float Heading
+    {
+        get; private set;
+    }
+    public float Speed
+    {
+        get; private set;
+    }
+    public float X
+    {
+        get
+        {
+            return Position.X;
+        }
+    }
+    public float Y
+    {
+        get
+        {
+            return Position.Y;
+        }
+    }
+    public float H
+    {
+        get
+        {
+            return this.Heading;
+        }
+    }
+    public float S
+    {
+        get
+        {
+            return this.Speed;
+        }
+    }
+    public Entity CreateNext(Vector2 deltaPosition, float deltaHeading)
+    {
+        return new Entity()
+        {
+            Position = this.Position + deltaPosition,
+            Appearance = this.Appearance,
+            Heading = this.Heading + deltaHeading
+        };
+    }
+}
