9dc7ebc6384e1d8d2c1cbb8b1bd02368a8524040
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2015-10-29 15:20:43 +0100

    Added inputcontror

---
+++ ourGame/ourGame/IGameInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ourGame
+{
+    enum RotationState
+    {
+        CCW,
+        CW,
+        NONE
+    }
+
+    interface IGameInput
+    {
+        RotationState CurrentRotationState { get; }
+        Boolean ShouldIncreaseSpeed { get;  }
+
+        Boolean ShouldDecreaseSpeed { get; }
+
+        void Update();
+    }
+}
---
+++ ourGame/ourGame/KeyboardInputController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace ourGame
+{
+    class KeyboardInputController : IGameInput
+    {
+        KeyboardState state;
+
+        public void Update()
+        {
+            state = Keyboard.GetState();
+
+        }
+
+        public RotationState CurrentRotationState { get {
+            if (state.IsKeyDown(Keys.A) && state.IsKeyDown(Keys.S)) {
+                return RotationState.NONE;
+            }
+            else {
+                if (state.IsKeyDown(Keys.A)) {
+                    return RotationState.CCW;
+                }
+                if(state.IsKeyDown(Keys.D)) {
+                    return RotationState.CW;
+                }
+            }
+            return RotationState.NONE;
+        } }
+
+        public Boolean ShouldIncreaseSpeed
+        {
+            get { return state.IsKeyDown(Keys.W); }
+        }
+
+        public Boolean ShouldDecreaseSpeed
+        {
+            get { return state.IsKeyDown(Keys.S); }
+        }
+
+    }
+}
