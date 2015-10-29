fc3a589765623cbf1f3ef6eaaf260153bfae4f2e
wouter van der plas <wouterv.dplas@gmail.com>
2015-10-29 15:21:02 +0100

    removed not needed code

--- ourGame/ourGame/Ship.cs
+++ ourGame/ourGame/Ship.cs
@@ -56,44 +56,8 @@ namespace ourGame {
             position.X = (int)x;            
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public void Draw(SpriteBatch spriteBatch) {
             spriteBatch.Draw(texture, position, sourceRectangle: null, color: Color.White, rotation: heading, origin: new Vector2(texture.Width / 2, texture.Height / 2),effects: SpriteEffects.None, layerDepth: 1.0f);
-            // TODO: Add your drawing code here
-        }
-
-        //getters + setters
-        public int getX() {
-            return position.X;
-        }
-
-        public int getY() {
-            return position.Y;
-        }
-
-        public int getWidth() {
-            return position.Width;
-        }
-
-        public int getHeight() {
-            return position.Height;
-        }
-
-        public float getHeading() {
-            return heading;
-        }
-
-        public float getProjectileSpeed()
-        {
-            return speed + cannonSpeed;
-        }
-
-        public Rectangle Position
-        {
-            get { return position; }
         }
     }
 }
